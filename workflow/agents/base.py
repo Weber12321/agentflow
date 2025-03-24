86b5e5671c139b6ff517e05c575b0bddac909d58
WeberTPI <weber.huang@tpisoftware.com>
2025-03-24 15:25:05 +0800

    rename workflow

--- workflow/agents/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from states.state import State
-
-
-class BaseAgent(ABC):
-
-    @abstractmethod
-    def run(self, state: State) -> State:
-        raise NotImplementedError
